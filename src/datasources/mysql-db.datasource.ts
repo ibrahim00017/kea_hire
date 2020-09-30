9eb695371f6c4c652d4f1a1f4e31fbfa4bf3e0cb
firmin <FirmiN88>
2020-09-30 16:03:09 +0100

    first commit

---
+++ src/datasources/mysql-db.datasource.ts
@@ -0,0 +1,31 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'MysqlDB',
+  connector: 'mysql',
+  url: 'mysql://root:passe@localhost/keahire_db',
+  host: 'localhost',
+  port: 0,
+  user: 'root',
+  password: 'passe',
+  database: 'keahire_db'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MysqlDbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'MysqlDB';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.MysqlDB', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,49 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class User extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstname: string;
+
+  @property({
+    type: 'string',
+  })
+  lastname?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone: string;
+
+  @property({
+    type: 'string',
+  })
+  email?: string;
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
